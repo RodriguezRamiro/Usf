40be4cefbcd9879044b72b7b9f61092297e4d296
AspiringGitHubDeveloper <147748953+AspiringGitHubDeveloper@users.noreply.github.com>
2024-02-20 14:13:51 -0500

    Add files via upload

---
+++ 18.3ajax-giphy-party.js
@@ -0,0 +1,39 @@
+
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+/* ajax result to add a gif*/
+function addGif(res){
+    let numResults = res.data.length;
+    if(numResults){
+        let randomIdx = Math.floor(Math.random()* numResults);
+        let $newCol = $("<div>" , {class: "col-md-4 col-12 mb-4"});
+        let $newGif = $("img",{ src: res.data[randomIdx].images.original.url, calss: "w-100" });
+
+        $newCol.append($newGif);
+        $gifArea.append($newCol);
+    }
+}
+
+//form submission -clear seacrbox and ajax call
+
+$("form").on("submit", async function(e){
+    e.preventDefault();
+    let searchTerm = $searchInput.val();
+    $searchInput.val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search",{
+        params: {
+            q: searchTerm,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+    addGif(response.data);
+});
+
+/*remove Gif*/
+$("#remove").on("click", function(){
+    $gifArea.empty();
+});
+
+
