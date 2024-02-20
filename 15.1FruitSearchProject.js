9badc5d3bcdf53d2ad524c842c6bae51d0b53b79
AspiringGitHubDeveloper <147748953+AspiringGitHubDeveloper@users.noreply.github.com>
2024-02-19 21:38:09 -0500

    Add files via upload

---
+++ 15.1FruitSearchProject.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let search = [];
+	const results = fruit.filter(fruit => fruit.toLowerCase().includes(searchTerm));
+		const searchTerm = document.getElementById('fruit').value;
+	// TODO
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+	const result = search();
+	showSuggestions(result, input.value);
+}
+
+function showSuggestions(results, inputVal) {
+	// TODO
+	suggestions.innerHTML = '';
+	results.forEach(result =>{
+		const listItem = document.createElement('li');
+		listItem.textContent = result;
+		suggestions.appendChild(listItem);
+	});
+
+}
+
+function useSuggestion(e) {
+	// TODO
+	input.value = e.target.textContent;
+	suggestions.innerHTML = '';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
