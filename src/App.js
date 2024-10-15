d0a40b122171a5eb69b8a8910c3c9e5e5feefde5
Ramiro Rodriguez <147748953+RodriguezRamiro@users.noreply.github.com>
2024-10-15 16:39:46 -0400

    42RevisedMadlibs
    revised and restructured code to fix errors and add missing files: 
     package.json at the madlibs
     public folder which contains the index.html needed by package.json to run the npm run start command.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React, {useEffect} from 'react';
-import Login from './components/Login';
-import Spotify from './components/Spotify';
-import { reducerCases } from './utils/Constants';
-import { useStateProvider } from './utils/StateProvider';
+import React from 'react';
+import './App.css';
+import Madlib from './Madlib'; // Import the Madlib component
 
-
-export default function App() {
-  const [{token}, dispatch ] = useStateProvider();
-  useEffect(()=> {
-    const hash = window.location.hash;
-    if(hash){
-      const token = hash.substring(1).split("&")[0].split("=")[1];
-      dispatch({type: reducerCases.SET_TOKEN, token});
-    }
-  }, [token, dispatch])
+function App() {
   return (
-    <div>
-      {token ? <Spotify /> : <Login/>};
+    <div className="App">
+      {/* Render the Madlib component */}
+      <Madlib />
     </div>
-  )
+  );
 }
+
+export default App;
